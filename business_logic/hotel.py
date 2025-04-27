dbaa826a1f1e88e4348f76a1ed80b249fe57d881
Kendric <181333067+kendricscoles@users.noreply.github.com>
2025-04-27 11:18:54 +0200

    Update hotel.py

--- business_logic/hotel.py
+++ business_logic/hotel.py
@@ -1,6 +1,9 @@
 class Hotel:
-    hotel_id: int
-    name: str
-    stars: int
-    address: Address
+    def __init__(self) -> None:
+        self.__hotel_dal = data_access.HotelDAL()
 
+    def create_hotel(self, name: str, stars: int, address: model.Address) -> model.Hotel:
+        return self.__hotel_dal.create_new_hotel(name, stars, address)
+
+    def read_hotel(self, hotel_id: int) -> model.Hotel:
+        return self.__hotel_dal.read_hotel_by_id(hotel_id)
