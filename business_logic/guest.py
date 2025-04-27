ab18b15f0829cdf6903d6dff709e0ffc4d99eb16
Kendric <181333067+kendricscoles@users.noreply.github.com>
2025-04-27 11:21:48 +0200

    Update guest.py

--- business_logic/guest.py
+++ business_logic/guest.py
@@ -1,1 +1,14 @@
+import os
 
+import model
+import data_access
+
+class Guest:
+    def __init__(self) -> None:
+        self.__guest_dal = data_access.GuestDAL()
+
+    def create_guest(self, first_name: str, last_name: str, email: str, address: model.Address) -> model.Guest:
+        return self.__guest_dal.create_new_guest(first_name, last_name, email, address)
+
+    def read_guest(self, guest_id: int) -> model.Guest:
+        return self.__guest_dal.read_guest_by_id(guest_id)
