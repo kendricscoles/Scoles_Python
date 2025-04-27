bc2f57c16fdfecdffe9e5a33553f8252890bcdf9
Kendric <181333067+kendricscoles@users.noreply.github.com>
2025-04-27 11:22:28 +0200

    Update invoice.py

--- business_logic/invoice.py
+++ business_logic/invoice.py
@@ -1,1 +1,14 @@
+import os
 
+import model
+import data_access
+
+class Invoice:
+    def __init__(self) -> None:
+        self.__invoice_dal = data_access.InvoiceDAL()
+
+    def create_invoice(self, issue_date, total_amount: float, booking: model.Booking) -> model.Invoice:
+        return self.__invoice_dal.create_new_invoice(issue_date, total_amount, booking)
+
+    def read_invoice(self, invoice_id: int) -> model.Invoice:
+        return self.__invoice_dal.read_invoice_by_id(invoice_id)
