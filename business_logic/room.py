5b9b92d5b1379ef250a0ea542213478672dd8f87
Kendric <181333067+kendricscoles@users.noreply.github.com>
2025-04-27 11:21:32 +0200

    Update room.py

--- business_logic/room.py
+++ business_logic/room.py
@@ -1,1 +1,14 @@
+import os
 
+import model
+import data_access
+
+class Room:
+    def __init__(self) -> None:
+        self.__room_dal = data_access.RoomDAL()
+
+    def create_room(self, room_no: str, price_per_night: float, room_type: model.RoomType, hotel: model.Hotel) -> model.Room:
+        return self.__room_dal.create_new_room(room_no, price_per_night, room_type, hotel)
+
+    def read_room(self, room_id: int) -> model.Room:
+        return self.__room_dal.read_room_by_id(room_id)
