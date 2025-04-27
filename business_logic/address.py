bc339add1bb337297d141bbc3809e6bda62ca70f
Kendric <181333067+kendricscoles@users.noreply.github.com>
2025-04-27 11:18:22 +0200

    Update address.py

--- business_logic/address.py
+++ business_logic/address.py
@@ -1,5 +1,9 @@
 class Address:
-    address_id: int
-    street: str
-    city: str
-    zip: str
+    def __init__(self) -> None:
+        self.__address_dal = data_access.AddressDAL()
+
+    def create_address(self, street: str, city: str, zip: str) -> model.Address:
+        return self.__address_dal.create_new_address(street, city, zip)
+
+    def read_address(self, address_id: int) -> model.Address:
+        return self.__address_dal.read_address_by_id(address_id)
