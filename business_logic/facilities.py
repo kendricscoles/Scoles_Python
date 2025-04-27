7b1fac1404dd35100e0525facb3c805475a27373
Kendric <181333067+kendricscoles@users.noreply.github.com>
2025-04-27 11:20:37 +0200

    Update facilities.py

--- business_logic/facilities.py
+++ business_logic/facilities.py
@@ -1,1 +1,14 @@
+import os
 
+import model
+import data_access
+
+class Facility:
+    def __init__(self) -> None:
+        self.__facility_dal = data_access.FacilityDAL()
+
+    def create_facility(self, facility_name: str) -> model.Facility:
+        return self.__facility_dal.create_new_facility(facility_name)
+
+    def read_facility(self, facility_id: int) -> model.Facility:
+        return self.__facility_dal.read_facility_by_id(facility_id)
