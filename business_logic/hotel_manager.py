69fecd7a1e9bede9809af57bc77583b5c282e43a
Kendric <181333067+kendricscoles@users.noreply.github.com>
2025-05-26 08:21:38 +0200

    Update and rename hotel.py to hotel_manager.py

--- business_logic/hotel_manager.py
+++ business_logic/hotel_manager.py
@@ -1,3 +1,8 @@
+import os
+
+import model
+import data_access
+
 class Hotel:
     def __init__(self) -> None:
         self.__hotel_dal = data_access.HotelDAL()
@@ -7,3 +12,6 @@ class Hotel:
 
     def read_hotel(self, hotel_id: int) -> model.Hotel:
         return self.__hotel_dal.read_hotel_by_id(hotel_id)
+        
+    def search_hotels(self,city=None, min_stars=None, max_guests=None, check_in_date=None, check_out_date=None):
+        return self.__hotel_dal.search_hotels(city, min_stars, max_guests, check_in_date, check_out_date)
