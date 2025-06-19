52d79e86345e69598a028de95a8ff14c954f952f
Kendric <181333067+kendricscoles@users.noreply.github.com>
2025-06-19 17:01:26 +0200

    Update hotel.py

--- model/hotel.py
+++ model/hotel.py
@@ -1,26 +1,11 @@
-from __future__ import annotations
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from model.address import Address
+from model.address import Address
 
 class Hotel:
-    def __init__(self, hotel_id: int, name: str, stars: int, address: Address = None):
-        if not hotel_id:
-            raise ValueError("hotel_id is required")
-        if not isinstance(hotel_id, int):
-            raise ValueError("hotel_id must be an integer")
-        if not name:
-            raise ValueError("name is required")
-        if not isinstance(name, str):
-            raise ValueError("name must be a string")
-        if not isinstance(stars, int):
-            raise ValueError("stars must be an integer")
-
+    def __init__(self, hotel_id: int, name: str, address: Address, stars: int):
         self.__hotel_id = hotel_id
         self.__name = name
-        self.__stars = stars
         self.__address = address
+        self.__stars = stars
 
     @property
     def hotel_id(self): return self.__hotel_id
@@ -29,10 +14,23 @@ class Hotel:
     @name.setter
     def name(self, value): self.__name = value
     @property
+    def address(self): return self.__address
+    @property
     def stars(self): return self.__stars
     @stars.setter
     def stars(self, value): self.__stars = value
-    @property
-    def address(self): return self.__address
-    @address.setter
-    def address(self, value): self.__address = value
+
+    def __repr__(self):
+        return f"<Hotel {self.name}, {self.stars}★>"
+
+    @classmethod
+    def from_row(cls, row, address: Address):
+        return cls(row[0], row[1], address, row[2])
+
+    def to_dict(self):
+        return {
+            "hotel_id": self.hotel_id,
+            "name": self.name,
+            "stars": self.stars,
+            "address": self.address.to_dict()
+        }
