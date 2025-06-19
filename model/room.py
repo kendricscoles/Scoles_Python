07aed4a15007bb95bfa2cc3fe92113872f233cd3
Kendric <181333067+kendricscoles@users.noreply.github.com>
2025-06-19 17:01:44 +0200

    Update room.py

--- model/room.py
+++ model/room.py
@@ -1,44 +1,37 @@
-from __future__ import annotations
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from model.hotel import Hotel
-    from model.room_type import RoomType
+from model.room_type import RoomType
+from model.hotel import Hotel
 
 class Room:
-    def __init__(self, room_id: int, room_no: str, price_per_night: float, room_type: RoomType, hotel: Hotel):
-        if not room_id or not isinstance(room_id, int):
-            raise ValueError("room_id must be an integer")
-        if not room_no or not isinstance(room_no, str):
-            raise ValueError("room_no must be a string")
-        if not isinstance(price_per_night, (float, int)):
-            raise ValueError("price_per_night must be a float")
-        
+    def __init__(self, room_id: int, room_number: str, price_per_night: float, hotel: Hotel, room_type: RoomType):
         self.__room_id = room_id
-        self.__room_no = room_no
-        self.__price_per_night = float(price_per_night)
-        self.__room_type = room_type
+        self.__room_number = room_number
+        self.__price_per_night = price_per_night
         self.__hotel = hotel
+        self.__room_type = room_type
 
     @property
     def room_id(self): return self.__room_id
-
     @property
-    def room_no(self): return self.__room_no
-    @room_no.setter
-    def room_no(self, value): self.__room_no = value
-
+    def room_number(self): return self.__room_number
     @property
     def price_per_night(self): return self.__price_per_night
-    @price_per_night.setter
-    def price_per_night(self, value): self.__price_per_night = float(value)
-
-    @property
-    def room_type(self): return self.__room_type
-    @room_type.setter
-    def room_type(self, value): self.__room_type = value
-
     @property
     def hotel(self): return self.__hotel
-    @hotel.setter
-    def hotel(self, value): self.__hotel = value
+    @property
+    def room_type(self): return self.__room_type
+
+    def __repr__(self):
+        return f"<Room {self.room_number}, {self.room_type.description}, {self.price_per_night:.2f} CHF>"
+
+    @classmethod
+    def from_row(cls, row, hotel: Hotel, room_type: RoomType):
+        return cls(row[0], row[1], row[2], hotel, room_type)
+
+    def to_dict(self):
+        return {
+            "room_id": self.room_id,
+            "room_number": self.room_number,
+            "price_per_night": self.price_per_night,
+            "hotel": self.hotel.to_dict(),
+            "room_type": self.room_type.to_dict()
+        }
