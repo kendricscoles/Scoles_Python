96ac5573084610a632537f152b200fc1a33e6d35
Kendric <181333067+kendricscoles@users.noreply.github.com>
2025-06-19 17:02:01 +0200

    Update room_type.py

--- model/room_type.py
+++ model/room_type.py
@@ -1,25 +1,26 @@
 class RoomType:
-    def __init__(self, room_type_id: int, description: str, max_guests: int):
-        if not room_type_id or not isinstance(room_type_id, int):
-            raise ValueError("room_type_id must be an integer")
-        if not description or not isinstance(description, str):
-            raise ValueError("description must be a string")
-        if not isinstance(max_guests, int):
-            raise ValueError("max_guests must be an integer")
-
-        self.__room_type_id = room_type_id
+    def __init__(self, type_id: int, description: str, max_guests: int):
+        self.__type_id = type_id
         self.__description = description
         self.__max_guests = max_guests
 
     @property
-    def room_type_id(self): return self.__room_type_id
-
+    def type_id(self): return self.__type_id
     @property
     def description(self): return self.__description
-    @description.setter
-    def description(self, value): self.__description = value
-
     @property
     def max_guests(self): return self.__max_guests
-    @max_guests.setter
-    def max_guests(self, value): self.__max_guests = value
+
+    def __repr__(self):
+        return f"<RoomType {self.description} (max {self.max_guests} guests)>"
+
+    @classmethod
+    def from_row(cls, row):
+        return cls(*row)
+
+    def to_dict(self):
+        return {
+            "type_id": self.type_id,
+            "description": self.description,
+            "max_guests": self.max_guests
+        }
